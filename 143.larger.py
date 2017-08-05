01bfa2016fc7a4265ee5e3c3821c42811d241778
teenTita <30741877+teenTita@users.noreply.github.com>
2017-08-05 11:02:30 +0530

    5th question

---
+++ 143.larger.py
@@ -0,0 +1,9 @@
+a=int(raw_input("enter the first number"))
+b=int(raw_input("enter the second number"))
+c=int(raw_input("enter the third number"))
+if a>b and a>c:
+    print "a is larger"
+elif b>a and b>c:
+    print "b is larger"
+else:
+    print "c is greater"
